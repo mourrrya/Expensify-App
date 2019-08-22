c38bdfc1a9129964a91ea70abcb2cada376bb508
anil kumar <mourrrya@gmail.com>
2019-08-22 18:00:32 +0530

    setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,59 @@
 const path = require('path')
-//entry point-->output point
-module.exports = {
-  mode: 'development',
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-            plugins: ["@babel/plugin-proposal-class-properties", "@babel/plugin-proposal-object-rest-spread"]
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const devMode = process.env.NODE_ENV !== 'production';
+
+module.exports = (env) => {
+  const isProduction = env === 'production'
+
+
+  return {
+    mode: 'development',
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+              plugins: ["@babel/plugin-proposal-class-properties", "@babel/plugin-proposal-object-rest-spread"]
+            }
           }
+        }, {
+          test: /\.s?css$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                hmr: process.env.NODE_ENV === 'development',
+                
+              },
+            },
+            'css-loader',
+            'sass-loader',
+          ],
         }
-      }, {
-        test: /\.s?css$/,
-        use: ['style-loader', 'css-loader','sass-loader']
-      }
-    ]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    historyApiFallback:true,
-    contentBase: path.join(__dirname, 'public')
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        // Options similar to the same options in webpackOptions.output
+        // both options are optional
+        filename: devMode ? '[name].css' : '[name].[hash].css',
+        chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+      }),
+    ],
+     devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      historyApiFallback:true,
+      contentBase: path.join(__dirname, 'public')
+    }
   }
 }
