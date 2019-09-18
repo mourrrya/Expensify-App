7db6bea9b31ebca2be3d941b2302030d41cd5306
anil kumar <mourrrya@gmail.com>
2019-09-19 03:44:37 +0530

    set private only routes

--- src/routers/router.js
+++ src/routers/router.js
@@ -4,23 +4,20 @@ import { createBrowserHistory } from "history";
 import AddExpense from "../components/addExpensePage";
 import Edit from "../components/editExpensePage";
 import ExpenseListPage from "../components/expenseListPage";
-import Header from "../components/header";
 import Help from "../components/help";
 import Login from "../components/loginPage";
 import PageNotFound from "../components/pageNotFound";
+import PrivateRoute from "../routers/privateRoute";
 
 export const history = createBrowserHistory();
 export default () => {
   const routes = (
     <Router history={history}>
-      <div>
-        <Header />
-      </div>
       <Switch>
         <Route path="/" component={Login} exact={true} />
-        <Route path="/expense" component={ExpenseListPage} />
-        <Route path="/create" component={AddExpense} />
-        <Route path="/edit/:id" component={Edit} />
+        <PrivateRoute path="/expense" component={ExpenseListPage} />
+        <PrivateRoute path="/create" component={AddExpense} />
+        <PrivateRoute path="/edit/:id" component={Edit} />
         <Route path="/help" component={Help} />
         <Route component={PageNotFound} />
       </Switch>
